c77148d847511a80ed74f46134d9d5e843aaa628
Yussuf <ggo353410@gmail.com>
2025-12-17 20:56:33 +0200

    admin, models, requirements

--- djangotutorial/privacy/models.py
+++ djangotutorial/privacy/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    notes = models.TextField(blank=True)
+
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    created_at= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
